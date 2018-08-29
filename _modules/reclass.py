6f47d53508c2c8da259baea60f00625cab7e5ab2
Vasyl Saienko <vsaienko@mirantis.com>
2018-08-29 09:04:43 +0000

    Revert "Fix, reclass module to get config from salt configuration"
    https://mirantis.jira.com/browse/PROD-22698
    
    This reverts commit d9a3d4bee03411f6e9cb8d7747796c41c1e6cd7c.
    
    Change-Id: I574dded9a637fe2f9a5252869af24004d2dd903c

--- _modules/reclass.py
+++ _modules/reclass.py
@@ -41,7 +41,7 @@ def _deps(ret_classes=True, ret_errors=False):
     '''
     Returns classes if ret_classes=True, else returns soft_params if ret_classes=False
     '''
-    defaults = __salt__['config.get']('reclass', None) or find_and_read_configfile()
+    defaults = find_and_read_configfile()
     path = defaults.get('inventory_base_uri')
     classes = {}
     soft_params = {}
@@ -120,12 +120,14 @@ def _deps(ret_classes=True, ret_errors=False):
 
 
 def _get_nodes_dir():
-    defaults = __salt__['config.get']('reclass', None) or find_and_read_configfile()
-    return defaults.get('nodes_uri') or os.path.join(defaults.get('inventory_base_uri'), 'nodes')
+    defaults = find_and_read_configfile()
+    return defaults.get('nodes_uri') or \
+        os.path.join(defaults.get('inventory_base_uri'), 'nodes')
+
 
 def _get_classes_dir():
-    defaults = __salt__['config.get']('reclass', None) or find_and_read_configfile()
-    return defaults.get('classes_uri') or os.path.join(defaults.get('inventory_base_uri'), 'classes')
+    defaults = find_and_read_configfile()
+    return os.path.join(defaults.get('inventory_base_uri'), 'classes')
 
 
 def _get_cluster_dir():
@@ -722,7 +724,7 @@ def validate_pillar(node_name=None, **kwargs):
                 ret.update(validate_pillar(node_name))
         return ret
     else:
-        defaults = __salt__['config.get']('reclass', None) or find_and_read_configfile()
+        defaults = find_and_read_configfile()
         meta = ''
         error = None
         try:
@@ -756,7 +758,7 @@ def node_pillar(node_name, **kwargs):
         salt-call reclass.node_pillar minion_id
 
     '''
-    defaults = __salt__['config.get']('reclass', None) or find_and_read_configfile()
+    defaults = find_and_read_configfile()
     pillar = ext_pillar(node_name, {}, defaults['storage_type'], defaults['inventory_base_uri'])
     output = {node_name: pillar}
 
@@ -773,7 +775,7 @@ def inventory(**connection_args):
 
         salt '*' reclass.inventory
     '''
-    defaults = __salt__['config.get']('reclass', None) or find_and_read_configfile()
+    defaults = find_and_read_configfile()
     storage = get_storage(defaults['storage_type'], _get_nodes_dir(), _get_classes_dir())
     reclass = Core(storage, None)
     nodes = reclass.inventory()["nodes"]
